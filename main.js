4ce5defa25f69dabf642a227564cf6ffe5dcfa5a
jaweria-kamran22 <kamranjaweria99@gmail.com>
2021-03-24 01:08:37 +0500

    Basic calculator

---
+++ main.js
@@ -0,0 +1,27 @@
+function writeTime() {
+  let time = document.getElementById("time").innerHTML;
+  let today = new date();
+  let time = today.getHours() + ":" + today.getMinutes();
+  time = document.write(time);
+}
+function calcNumber(result) {
+  document.querySelector(".area").value += result;
+}
+function percentage(result) {
+  let result = String(result);
+  if (result1.includes("%")) {
+    let a, b, percent;
+    let splitWord = [];
+    a = Number(splitWord[0]);
+    b = Number(splitWord[1]);
+    percent = (a / 100) * b;
+    document.querySelector(".area").value = percent;
+  }
+}
+function makeNegative(result) {
+  if (result.charAt(0) == "_") {
+    document.querySelector(".area").value = result.replace("-", "");
+  } else {
+    document.querySelector(".area").value = "-" + result;
+  }
+}
